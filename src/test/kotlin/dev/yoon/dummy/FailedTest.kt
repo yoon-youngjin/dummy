67db2767a08672f9976a3296148a4c56c13cb064
yoon-youngjin <dudwls143@gmail.com>
2025-08-09 18:14:05 +0900

    fail test 제거

--- src/test/kotlin/dev/yoon/dummy/FailedTest.kt
+++ src/test/kotlin/dev/yoon/dummy/FailedTest.kt
@@ -1,10 +1,12 @@
 package dev.yoon.dummy
 
+import org.junit.jupiter.api.Disabled
 import kotlin.test.Test
 import kotlin.test.fail
 
 class FailedTest {
     @Test
+    @Disabled
     fun alwaysFails() {
         fail("이 테스트는 무조건 실패합니다.")
     }
