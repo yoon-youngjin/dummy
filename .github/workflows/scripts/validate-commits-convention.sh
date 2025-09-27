fb17b9b0a524faac08a3020b2cba498bab4b8b9c
yoon-youngjin <dudwls143@gmail.com>
2025-09-27 17:04:30 +0900

    feature: test

--- .github/workflows/scripts/validate-commits-convention.sh
+++ .github/workflows/scripts/validate-commits-convention.sh
@@ -4,9 +4,9 @@ set -euo pipefail
 BASE_REF="${1}"
 HEAD_REF="${2}"
 
-git fetch --no-tags --prune --depth=0 origin || true
-
 CONVENTION_REGEX='^(feature|fix|refactor|test|revert)(\([a-z0-9._-]+\))?!?: .+'
+
+git log --no-merges --pretty=%s "${BASE_REF}..${HEAD_REF}"
 
 mapfile -t COMMITS < <(git log --no-merges --pretty=%s "${BASE_REF}..${HEAD_REF}")
 
